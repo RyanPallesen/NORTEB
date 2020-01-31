eaca9ac4a7a57d0942b2d9f628edf8e92034b621
RyanPallesen <pallesenryan@gmail.com>
2020-01-31 21:38:06 +1100

    Updated to 2019.2

---
+++ NORTTEB/Assets/Scripts/Cards/BaseCard.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class BaseCard : ScriptableObject
+{
+    public enum BaseCardType
+    {
+        Resource,
+        Event,
+        Action
+    }
+
+    [HideInInspector] public BaseCardType baseCardType;
+
+    public string cardName;
+    public string cardDescription;
+}
---
+++ NORTTEB/Assets/Scripts/GridModule/TetrisPiece.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TetrisPiece : ScriptableObject {
+
+    public enum ResourceType
+    {
+    Air,
+    Metal,
+    Fuel
+    }
+
+    struct Square
+    {
+        int xOffset;
+        int yOffset;
+
+        [HideInInspector] public ResourceType resourceType;
+    }
+
+}
