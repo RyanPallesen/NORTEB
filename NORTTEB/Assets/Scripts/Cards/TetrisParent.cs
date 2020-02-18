cfd323f3c4fe8233b2871ba8c5d37a1f18eddf85
ryan pallesen <pallesenryan@gmail.com>
2020-02-18 22:35:27 +1100

    Began project restart

--- NORTTEB/Assets/Scripts/Cards/TetrisParent.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class TetrisParent : MonoBehaviour
-{
-    public bool isRepair = false;
-    public List<TetrisPiece.Square> squares = new List<TetrisPiece.Square>();
-}
